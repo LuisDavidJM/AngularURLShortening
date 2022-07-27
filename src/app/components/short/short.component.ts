28d21ed65676d2101dee88d20fa2fc67d2f17054
Luis David Mac <luisdavidjma@gmail.com>
2022-07-26 19:28:47 -0500

    Functional without styles

--- src/app/components/short/short.component.ts
+++ src/app/components/short/short.component.ts
@@ -9,27 +9,73 @@ import { ShortsService } from '../../services/shorts.service';
 })
 export class ShortComponent implements OnInit {
 
-  public shortLink: any;
+  public shortLink: any[] = [];
   public link: any;
 
   constructor(
     private shortsService: ShortsService
   ) {
     this.link = '';
+    this.getLocalStorage();
    }
 
   ngOnInit(): void {
 
   }
-  showLink(){
+  getLink(){
     this.shortsService.getUrl(this.link).subscribe(
       result => {
-        this.shortLink = result.result;
+        this.shortLink.unshift(result.result);
+        localStorage.setItem('localStorage', JSON.stringify(this.shortLink));
+        this.getLocalStorage();
       },
       error => {
-        console.log(<any>error);
+        console.log('<any>error');
       }
     );
+    this.inputReset();
+  }
+  getLocalStorage(){
+    var local = localStorage.getItem('localStorage');
+        if(local == null){
+          this.shortLink = [];
+        }else{
+          this.shortLink = JSON.parse(local);
+        }  
+  }
+  inputReset(){
+    const input = document.getElementById('input') as HTMLInputElement | null;
+    if(input != null){
+      input.value = '';
+    }   
+  }
+  copyLink(num: any){
+    var copy = document.querySelector(`.copy${num.code}`)?.innerHTML;
+    if(copy != undefined){
+      navigator.clipboard.writeText(copy)
+    .then(() => {
+
+    })
+    .catch(err => {
+      console.log('Error', err);
+    });
+    }
   }
 
+  clearLinks(){
+    this.shortLink = [];
+    localStorage.clear();
+  }
+
+  getLinkValidation(){
+    var val: any = document.getElementById('input') as HTMLInputElement | null;
+    if(val != undefined){
+      val = val.value;
+    }
+    if (val?.length > 0){
+      this.getLink();
+    }else{
+      console.log(val, 'Error')
+    }
+  }
 }
